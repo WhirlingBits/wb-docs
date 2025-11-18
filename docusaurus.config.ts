f1d4317f455fa514850ef6fd6669265e46323217
matti122 <matti122@whirlingbits.de>
2025-11-18 09:02:12 +0000

    fix: update copyright statement to include package version in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -499,7 +499,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} ${settings.branding.organizationName}. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} ${settings.branding.organizationName}. Version ${require('./package.json').version}`,
     },
     
     prism: {
